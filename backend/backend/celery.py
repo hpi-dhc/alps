095466e980f1ad20fe0ecfa292e26cf64b892092
Felix Musmann <felix.musmann@student.hpi.de>
2019-10-17 13:36:17 +0200

    Add ptvsd to celery

--- backend/backend/celery.py
+++ backend/backend/celery.py
@@ -2,11 +2,15 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
+import ptvsd
+ptvsd.enable_attach(address=('0.0.0.0', 5678), redirect_output=True)
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 app = Celery('backend')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+
 
 @app.task(bind=True)
 def debug_task(self):
